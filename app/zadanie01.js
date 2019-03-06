c3d62b2cb16638dd02fbc9a8a89d9253c5754510
Michał Romańczuk <roman@sawblade.pl>
2019-03-06 18:53:30 +0100

    first task done

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,17 @@
 //Twój kod
+
+const express = require('express');
+const srv = express();
+const bodyParser = require('body-parser');
+srv.use(express.static('./public/zadanie01'));
+srv.use(bodyParser.urlencoded());
+
+srv.post('/form/submit', (req, res) => {
+    res.send(`${req.body['secondNumber']} ${req.body['firstNumber']%req.body['secondNumber']==0?'':'nie'} jest dzielnikiem ${req.body['firstNumber']}`);
+});
+
+
+srv.listen(3000, () => {
+    console.log('listening on port 3000');
+})
+
