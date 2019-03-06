e8b9172f7c6cf168aee65bda3f76f4ed1f553037
Michał Romańczuk <roman@sawblade.pl>
2019-03-06 22:48:49 +0100

    done

--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,33 @@
 //Twój kod
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const srv = express();
+
+srv.use(express.static('./public/zadanie02'));
+srv.use(cookieParser());
+srv.use(bodyParser.urlencoded());
+
+srv.post('/cookie/set', (req, res) => {
+    let name = req.body['name'];
+    console.log(`Entered name ${name}`);
+    res.cookie('name', name, {
+        maxAge: 31536000000
+    });
+    res.send('Name set');
+    console.log(`Name set to ${req.cookies.name}`);
+});
+
+srv.get('/cookie/show', (req, res) => {
+    res.send(`Name set to ${req.cookies.name}`);
+})
+
+srv.get('/cookie/check', (req, res) => {
+    res.send(`Cookie is ${req.cookies.name!=undefined?'':'not'} set`);
+})
+
+srv.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,33 @@
 //Twój kod
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const srv = express();
+
+
+srv.use(cookieParser());
+srv.use(bodyParser.urlencoded());
+
+srv.get('/', (req, res)=> {
+    let comments = JSON.parse(req.cookies.comments);
+    res.send(`${comments}<br><a href='/add'>Add a comment</a>`);
+})
+srv.use('/add', express.static('./public/zadanieDnia/add.html'));
+srv.post('/save', (req, res) => {
+    let cookie = getCommentsCookie(req.cookies.comments);
+    cookie.push(req.body['comment']);
+    res.cookie('comments', JSON.stringify(cookie));
+    
+    res.send(`Comment sent.<br><a href='/'>Return</a>`)
+});
+
+srv.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
+
+function getCommentsCookie(cookie) {
+    return cookie?JSON.parse(cookie):[];
+}
